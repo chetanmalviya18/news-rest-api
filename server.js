f6e2734acb170047f8f00e96b7e74aea4c275923
chetanmalviya18 <chetanmalviya.182@gmail.com>
2024-09-23 21:35:10 +0530

    Api rate limit added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import ApiRoutes from "./routes/api.js";
 import fileupload from "express-fileupload";
 import helmet from "helmet";
 import cors from "cors";
+import { limiter } from "./config/rateLimiter.js";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.static("public"));
 app.use(fileupload());
 app.use(helmet());
 app.use(cors());
+app.use(limiter);
 
 app.get("/", (req, res) => {
   return res.json({ message: "Hello it's News API" });
