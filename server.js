fbddfe96d22d3b83c28fed97d7719b8ebe086d45
chetanmalviya18 <chetanmalviya.182@gmail.com>
2024-09-23 21:24:05 +0530

    secure http header done

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import ApiRoutes from "./routes/api.js";
 import fileupload from "express-fileupload";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(fileupload());
+app.use(helmet());
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.json({ message: "Hello it's News API" });
